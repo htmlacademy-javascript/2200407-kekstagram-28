ae5511976b53bc92268434c8ea4cea8b5e2ca1f6
AnnaMos8 <moskvina.hanna@gmail.com>
2023-02-22 23:11:34 +0300

    Правки по линтеру

--- js/function.js
+++ js/function.js
@@ -5,23 +5,23 @@ const checkStringLength = (str, stringLength) => {
   }
 
   return false;
-}
+};
 
 // Функция для проверки, является ли строка палиндромом
 const isPalindrome = (str) => {
   let check = '';
-  for (let i = str.length; i > 0 ; i--) {
-    if (str[i - 1] != ' ') {
+  for (let i = str.length; i > 0; i--) {
+    if (str[i - 1] !== ' ') {
       check += str[i - 1];
     }
   }
 
-  if (str.split(' ').join('').toLowerCase() == check.toLowerCase()) {
+  if (str.split(' ').join('').toLowerCase() === check.toLowerCase()) {
     return true;
   }
 
   return false;
-}
+};
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 const extractNumbers = (str) => {
@@ -32,14 +32,14 @@ const extractNumbers = (str) => {
     str = String(str);
   }
 
-  for (let i = 0; i < str.length ; i++) {
-    if (!isNaN(str[i]) && str[i]!=' ') {
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i]) && str[i] !== ' ') {
       result += str[i];
     }
   }
 
   return parseInt(result, 10);
-}
+};
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки.
 const myPadStart = (str, minLength, pad) => {
@@ -48,16 +48,14 @@ const myPadStart = (str, minLength, pad) => {
   while (result.length < minLength) {
     const newResultLength = result.length + pad.length;
     const actualPad = newResultLength <= minLength ? pad :
-    pad.slice(0, minLength - newResultLength);
+      pad.slice(0, minLength - newResultLength);
     result = actualPad + result;
   }
 
   return result;
-}
+};
 
-/* console.log(myPadStart('1', 2, '0'));
-console.log(myPadStart('1', 4, '0'));
-console.log(myPadStart('q', 4, 'werty'));
-console.log(myPadStart('q', 4, 'we'));
-console.log(myPadStart('qwerty', 4, '0'));
-*/
+checkStringLength('проверяемая строка', 18);
+isPalindrome('Лёша на полке клопа нашёл ');
+extractNumbers('1 кефир, 0.5 батона');
+myPadStart('q', 4, 'we');
